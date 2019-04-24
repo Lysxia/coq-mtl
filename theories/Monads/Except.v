e43391755473249602b91d135c98307208b93f33
Lysxia <lysxia@gmail.com>
2019-04-24 07:30:28 -0400

    Verify that ExceptT is a lawful MonadState

--- theories/Monads/Except.v
+++ theories/Monads/Except.v
@@ -1,3 +1,6 @@
+(** * The exception monad transformer *)
+
+From Coq Require Import FunctionalExtensionality.
 From mtl.Classes Require Import All.
 
 Implicit Types e : Type.
@@ -9,7 +12,9 @@ Record ExceptT e m (a : Type) : Type :=
 Arguments MkExceptT {e m a} _.
 Arguments runExceptT {e m a}.
 
-Instance Monad_ExceptT e m `{Monad m} : Monad (ExceptT e m) :=
+(** ** Instances *)
+
+Instance Monad_ExceptT {e m} `{Monad m} : Monad (ExceptT e m) :=
   {| pure _ x := MkExceptT (pure (inr x))
    ; bind _ _ u k := MkExceptT (
        runExceptT u >>= fun x' =>
@@ -19,7 +24,44 @@ Instance Monad_ExceptT e m `{Monad m} : Monad (ExceptT e m) :=
        end)
   |}.
 
-Instance MonadTrans_ExceptT e : MonadTrans (ExceptT e) :=
+Instance MonadTrans_ExceptT {e} : MonadTrans (ExceptT e) :=
   {| lift _ _ _ u := MkExceptT (u >>= fun x => pure (inr x))
   |}.
 
+Instance MonadState_ExceptT {e s m} `{Monad m} `{MonadState s m} :
+  MonadState s (ExceptT e m) :=
+  MonadState_MonadTrans.
+
+(** ** Simple facts *)
+
+Lemma injective_runExceptT {e m a} (u v : ExceptT e m a) :
+  runExceptT u = runExceptT v -> u = v.
+Proof.
+  destruct u, v; intros; f_equal; auto.
+Qed.
+
+(** ** Laws *)
+
+Instance LawfulMonad_ExceptT {e m} `{LawfulMonad m} : LawfulMonad (ExceptT e m).
+Proof.
+  split.
+  all: intros; apply injective_runExceptT; cbn.
+  - rewrite bind_pure_l. reflexivity.
+  - transitivity (runExceptT u >>= pure).
+    + f_equal; apply functional_extensionality; intros []; auto.
+    + rewrite bind_pure_r. reflexivity.
+  - rewrite bind_assoc; f_equal; apply functional_extensionality; intros []; auto.
+    rewrite bind_pure_l; reflexivity.
+Qed.
+
+Instance LawfulMonadTrans_ExceptT {e} : LawfulMonadTrans (ExceptT e).
+Proof.
+  split; intros; cbn.
+  - rewrite bind_pure_l; reflexivity.
+  - rewrite !bind_assoc; do 2 f_equal; apply functional_extensionality; intros.
+    rewrite bind_pure_l; reflexivity.
+Qed.
+
+Instance LawfulMonadState_ExceptT {e s m} `{LawfulMonad m} `{MonadState s m}
+  {_ : LawfulMonadState s m} : LawfulMonadState s (ExceptT e m) :=
+  LawfulMonadState_LawfulMonadTrans.

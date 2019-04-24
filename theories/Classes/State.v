0dceb3037e3e6f7892b0ae589685967ac41547f1
Lysxia <lysxia@gmail.com>
2019-04-23 21:14:17 -0400

    Prove MonadState laws for transformers

--- theories/Classes/State.v
+++ theories/Classes/State.v
@@ -1,10 +1,7 @@
-From mtl.Classes Require Import Monad.
+From mtl.Classes Require Import Monad Trans.
 From Coq Require Import Setoid FunctionalExtensionality Relations Morphisms.
 
-Instance sss {a b} : subrelation (eq ==> eq)%signature (@eq (a -> b)).
-Proof.
-Admitted.
-
+Implicit Types t : (Type -> Type) -> (Type -> Type).
 Implicit Types m : Type -> Type.
 Implicit Types s : Type.
 
@@ -26,10 +23,11 @@ Class LawfulMonadState s m `{Monad m} `{MonadState s m} : Prop :=
   { get_put :   get    >>= put   = pure tt
   ; put_get : `(put z  >> get    = put z >> pure z)
   ; put_put : `(put z1 >> put z2 = put z2)
-  ; get_get : forall a (k : s -> s -> m a),
-      (get >>= fun z1 => get >>= fun z2 => k z1 z2) = (get >>= fun z => k z z)
+  ; get_get :
+        (get >>= fun z1 => get >>= fun z2 => pure (z1, z2))
+      = (get >>= fun z => pure (z, z))
     (* [get] has no side effect. *)
-  ; nullipotent_get : forall a (u : m a), get >> u = u
+  ; nullipotent_get : get >> pure tt = pure tt
   }.
 
 Section StateFacts.
@@ -38,6 +36,34 @@ Context s m.
 Context `{Monad m} `{MonadState s m}.
 Context {LM : LawfulMonad m}.
 Context {LMS : LawfulMonadState s m}.
+
+(** Variants of [get_get] and [nullipotent_get] which quantify over the final
+    continuation. They are equivalent, but a bit less convenient to prove directly
+    when lifting [MonadState] through transformers.
+  *)
+
+Lemma get_get_k : forall a (k : s -> s -> m a),
+    (get >>= fun z1 => get >>= fun z2 => k z1 z2)
+  = (get >>= fun z => k z z).
+Proof.
+  intros.
+  transitivity ((get >>= fun z => pure (z, z)) >>= fun '(z1, z2) => k z1 z2).
+  - rewrite <- get_get.
+    repeat (rewrite bind_assoc; f_equal; apply functional_extensionality; intros).
+    rewrite bind_pure_l; reflexivity.
+  - rewrite bind_assoc; f_equal; apply functional_extensionality; intros.
+    rewrite bind_pure_l; reflexivity.
+Qed.
+
+Lemma nullipotent_get_k : forall a (u : m a),
+  get >> u = u.
+Proof.
+  intros.
+  transitivity (get >> pure tt >> u).
+  - rewrite bind_assoc; f_equal; apply functional_extensionality; intros.
+    rewrite bind_pure_l; reflexivity.
+  - rewrite nullipotent_get, bind_pure_l; reflexivity.
+Qed.
 
 Lemma get_state : get = state (fun z => (z, z)).
 Proof.
@@ -51,7 +77,7 @@ Qed.
 Lemma put_state : `(put z = state (fun _ => (tt, z))).
 Proof.
   intros; unfold state.
-  rewrite nullipotent_get.
+  rewrite nullipotent_get_k.
   replace (fun _ => pure tt) with (fun t : unit => pure t).
   2: apply functional_extensionality; intros []; auto.
   rewrite bind_pure_r.
@@ -59,3 +85,35 @@ Proof.
 Qed.
 
 End StateFacts.
+
+Definition MonadState_MonadTrans {s t m} `{MonadTrans t} `{Monad m} `{MonadState s m} :
+  MonadState s (t m) :=
+  {| get := lift get
+   ; put z := lift (put z)
+  |}.
+
+Theorem LawfulMonadState_LawfulMonadTrans {s t m}
+  `{MonadTrans t}
+  `{forall m, Monad m -> Monad (t m)}
+  `{LawfulMonadTrans t}
+  `{LawfulMonad m}
+  `{MonadState s m}
+  {_ : LawfulMonadState s m} :
+  @LawfulMonadState s (t m) _ MonadState_MonadTrans.
+Proof.
+  split; cbn; intros; rewrite <- ?morphism_bind.
+  - rewrite get_put, morphism_pure; reflexivity.
+  - rewrite put_get, morphism_bind; f_equal;
+    apply functional_extensionality; intros; apply morphism_pure.
+  - rewrite put_put; reflexivity.
+  - transitivity (lift (get >>= fun z => pure (z, z))).
+    + rewrite <- get_get.
+      do 2 (rewrite morphism_bind; f_equal; apply functional_extensionality; intros).
+      rewrite morphism_pure; reflexivity.
+    + rewrite morphism_bind; f_equal; apply functional_extensionality; intros.
+      rewrite morphism_pure; reflexivity.
+  - transitivity (lift (get >> pure tt)).
+    + rewrite morphism_bind; f_equal; apply functional_extensionality; intros.
+      rewrite morphism_pure; reflexivity.
+    + rewrite nullipotent_get, morphism_pure; reflexivity.
+Qed.

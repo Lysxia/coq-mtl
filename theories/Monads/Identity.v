51cd3ebff7007dec6c93e019b61cd44dbae0241a
Lysxia <lysxia@gmail.com>
2019-05-12 12:07:24 -0400

    Prove lawfulness of Identity monad

--- theories/Monads/Identity.v
+++ theories/Monads/Identity.v
@@ -6,7 +6,18 @@ Record Identity (a : Type) : Type :=
 Arguments MkIdentity {a} _.
 Arguments runIdentity {a} _.
 
+Lemma injective_runIdentity {a} (u v : Identity a)
+  : runIdentity u = runIdentity v -> u = v.
+Proof.
+  destruct u, v; intros; f_equal; auto.
+Qed.
+
 Instance Monad_Identity : Monad Identity :=
   { pure _ := MkIdentity
   ; bind _ _ u k := k (runIdentity u)
   }.
+
+Instance LawfulMonad_Identity : LawfulMonad Identity.
+Proof.
+  split; intros; apply injective_runIdentity; reflexivity.
+Qed.
